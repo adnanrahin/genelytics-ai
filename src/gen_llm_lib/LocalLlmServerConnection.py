73120f5dcc1d8211079c1f2e7ab304be0b958f11
Adnan Habib Rahin <adnan.rahin13@gmail.com>
2024-04-30 22:02:09 -0400

    updated the models

--- src/gen_llm_lib/LocalLlmServerConnection.py
+++ src/gen_llm_lib/LocalLlmServerConnection.py
@@ -1,11 +1,15 @@
-from langchain_community.utilities import SQLDatabase
-from langchain_community.llms import OpenAI
 from config.aid_lib import load_database_prompts
-from config.mysql import MySqlMetaDataLoader
+from operator import itemgetter
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables import RunnablePassthrough
 from langchain_community.utilities import SQLDatabase
 from langchain.chains import create_sql_query_chain
-from langchain_core.prompts import FewShotPromptTemplate, PromptTemplate
+from langchain_core.prompts import (ChatPromptTemplate,
+                                    FewShotChatMessagePromptTemplate)
 from langchain_community.llms import Ollama
+from langchain_community.vectorstores import FAISS
+from langchain_core.example_selectors import SemanticSimilarityExampleSelector
+from langchain_community.embeddings import OllamaEmbeddings
 
 
 class LocalLLMServerConnection:
@@ -13,6 +17,7 @@ class LocalLLMServerConnection:
         self.llm = Ollama(base_url='http://192.168.1.248:11434', model="mistral_open_orca_7b")
         self.user_prompt = user_prompt
         self.db = db
+        self.history = ChatMessageHistory()
 
     def get_context(self):
         return self.db.get_context()
@@ -23,32 +28,59 @@ class LocalLLMServerConnection:
     def create_prompt(self):
         context = self.get_context()
         training_prompts = load_database_prompts('nasa_database_prompt.json')
-        example_prompt = PromptTemplate.from_template("User input: {input}\nSQL query: {query}")
-        prompt = FewShotPromptTemplate(
-            examples=training_prompts,
-            example_prompt=example_prompt,
-            prefix="You are a Mysql expert. Given an input question, create a syntactically correct Mysql query to run."
-                   "Unless otherwise specified, do not return more than {top_k} rows.\n\nHere is the relevant "
-                   "table info: {table_info}\n\nBelow are a number of examples of questions and their corresponding "
-                   "SQL queries.",
-            suffix="User input: {input}\nSQL query: ",
-            input_variables=["input", "top_k", "table_info"],
+        example_selector = SemanticSimilarityExampleSelector.from_examples(
+            training_prompts,
+            OllamaEmbeddings(base_url='http://192.168.1.248:11434', model="mistral_open_orca_7b"),
+            FAISS,
+            k=5,
+            input_keys=["input"],
         )
-        prompt.format(
+        example_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("human", "{input}\nSQLQuery:"),
+                ("ai", "{query}"),
+            ]
+        )
+        few_shot_prompt = FewShotChatMessagePromptTemplate(
+            example_prompt=example_prompt,
+            example_selector=example_selector,
+            input_variables=["input", "top_k"],
+        )
+
+        system_prompt = (
+            "You are a MySQL expert. Given an input question, create a syntactically correct MySQL query to run."
+            "Unless otherwise specified. Here is the relevant table info: {table_info}. Below are a number of "
+            "examples of questions and their corresponding SQL queries. Also use the {messages} to answer any "
+            "followup questions. In case of users wants to know how to answer a followup question or maybe fixing "
+            "error, use {messages} to"
+            "to Answer the followup question.")
+
+        final_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", system_prompt),
+                few_shot_prompt,
+                ("human", "{input}"),
+            ]
+        )
+        final_prompt.format(
             input=self.user_prompt,
             top_k=5,
-            table_info=context["table_info"]
+            table_info=context["table_info"],
+            messages=self.history.messages
         )
 
     def generate_sql_query(self):
-        chain = self.create_query_chain(self.create_prompt())
-        response = chain.invoke(
-            {
-                "question": self.user_prompt
-            }
+        generate_query = self.create_query_chain(self.create_prompt())
+        chain = (
+            RunnablePassthrough.assign(query=generate_query).assign(
+                result=itemgetter("query")
+            )
         )
+        response = chain.invoke({"question": self.user_prompt, "messages": self.history.messages})
+        self.history.add_user_message(self.user_prompt)
+        self.history.add_ai_message(response['result'])
         return response
 
     def process_user_input(self):
         response = self.generate_sql_query()
-        return response
+        return response['query']
